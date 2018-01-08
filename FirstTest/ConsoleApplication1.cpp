2862e2eb561084316ffc5a88c3d98f53a2a66c76
nnnik <nnnik@web.de>
2018-01-08 17:38:09 +0100

    Added custom hash and hash measurement

--- FirstTest/ConsoleApplication1.cpp
+++ FirstTest/ConsoleApplication1.cpp
@@ -4,38 +4,58 @@
 #include "stdafx.h"
 #include <iostream>
 #include <unordered_map>
+#include <functional>
 #include <string>
 #include <fstream>
 #include <Windows.h>
 
 using namespace std;
 
-unordered_map<string, int> readWordsFromLex(const string fileName) {
-	unordered_map< string, int > myMap = {};
-	fstream lexikon("allWordsWithB.lex");
+#define HASH_FN function<unsigned long(std::string)>
+#define HASH_MAP unordered_map<string, unsigned long, HASH_FN>
+
+
+
+HASH_MAP readWordsFromLex(const string fileName, HASH_FN hashFn ) {
+	HASH_MAP myMap( 0, hashFn );
+	fstream lexikon(fileName);
 	string word;
 	while (getline(lexikon, word)) {
-		myMap[word]++;
+		myMap[word] = hashFn(word);
 	}
 	return myMap;
 }
 
-int main()
-{
+LARGE_INTEGER measureAccessTimes(HASH_MAP map, string key, int times) {
 	LARGE_INTEGER Freq;
 	LARGE_INTEGER StartTime, EndTime, ElapsedTime;
 	QueryPerformanceFrequency(&Freq);
-	unordered_map<string, int> map = readWordsFromLex( "allWordsWithB.txt" );
+	QueryPerformanceCounter(&StartTime);
+	for (int i = 0; i < times; i++) {
+		int b = map[key];
+	}
+	QueryPerformanceCounter(&EndTime);
+	ElapsedTime.QuadPart = EndTime.QuadPart - StartTime.QuadPart;
+	ElapsedTime.QuadPart *= 1000000;
+	ElapsedTime.QuadPart /= Freq.QuadPart;
+	return ElapsedTime;
+}
+
+unsigned long hashing_func(string key) {
+	unsigned long hash = 0;
+	for (int i = 0; i<key.size(); i++)
+	{
+		hash += (71 * hash + key[i]) % 5;
+	}
+	return hash;
+}
+
+int main()
+{
+	HASH_MAP map = readWordsFromLex("../../../Lexika/allWordsWithB.txt", hashing_func);
+	cout << map.size();
 	ofstream log("test1.log");
-	for( int k = 0; k<3; k++ ) {
-		QueryPerformanceCounter(&StartTime);
-		for (int i = 0; i < 2000000000; i++) {
-			int b = map["BZZ"];
-		}
-		QueryPerformanceCounter(&EndTime);
-		ElapsedTime.QuadPart = EndTime.QuadPart - StartTime.QuadPart;
-		ElapsedTime.QuadPart *= 1000000;
-		ElapsedTime.QuadPart /= Freq.QuadPart;
-		log << ElapsedTime.QuadPart << "\n";
+	for( int k = 0; k<7; k++ ) {
+		log << measureAccessTimes( map, "BZZ", 2000000 ).QuadPart << "\n";
 	}
 }
