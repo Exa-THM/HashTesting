18bb458835fc98fd7b7482c415e4c2542d4e5b00
nnnik <nnnik@web.de>
2018-01-09 00:16:39 +0100

    Added measure Collisions

--- FirstTest/ConsoleApplication1.cpp
+++ FirstTest/ConsoleApplication1.cpp
@@ -2,11 +2,12 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
 #include <unordered_map>
 #include <functional>
 #include <string>
+#include <cmath>
 #include <fstream>
+#include <iostream>
 #include <Windows.h>
 
 using namespace std;
@@ -41,6 +42,18 @@ LARGE_INTEGER measureAccessTimes(HASH_MAP map, string key, int times) {
 	return ElapsedTime;
 }
 
+double measureCollisions(HASH_MAP map, int modulo) { //gibt die Standardabweichung der Verteilung der Inhalte aus Map auf modulo Buckets zurück
+	int* buckets = new int[ modulo ];
+	for (int i = 0; i < modulo; i++) buckets[i] = 0;
+	for (const auto& indices : map) buckets[indices.second % modulo]++;
+	double expectedAverage = (double)map.size() / (double)modulo;
+	double strayExpectedAverage = 0;
+	for (int i = 0; i < modulo; i++) strayExpectedAverage += pow((double)buckets[ i ] - expectedAverage, 2);
+	strayExpectedAverage /= modulo;
+	delete[] buckets;
+	return sqrt(strayExpectedAverage);
+}
+
 unsigned long hashing_func(string key) {
 	unsigned long hash = 0;
 	for (int i = 0; i<key.size(); i++)
@@ -52,10 +65,11 @@ unsigned long hashing_func(string key) {
 
 int main()
 {
-	HASH_MAP map = readWordsFromLex("../../../Lexika/allWordsWithB.txt", hashing_func);
+	HASH_MAP map = readWordsFromLex("../../../Lexika/FullLex.txt", hashing_func);
 	cout << map.size();
 	ofstream log("test1.log");
-	for( int k = 0; k<7; k++ ) {
+	for( int k = 0; k<20; k++ ) {
 		log << measureAccessTimes( map, "BZZ", 2000000 ).QuadPart << "\n";
 	}
+	for (int i = 1; i < map.size(); i++) log << "Collisions: " << measureCollisions( map, i ) << "\n";
 }
